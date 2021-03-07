3c545a984df914b4a76933527ed8b1e986883844
ismaj7 <71702877+ismaj7@users.noreply.github.com>
2021-03-07 12:25:59 +0100

    Intento de hacerlo orientado a objetos, pero no funciona.

--- src/Criba.java
+++ src/Criba.java
@@ -2,44 +2,82 @@ import java.util.Scanner;
 
 public class Criba {
 	
-    // Generar números primos de 1 a maxNumber
-    public static int[] generarPrimos (int maxNumber) {
-    	
-    	// Declaraciones 
-        int arrSize = maxNumber + 1; // Tamaño del array
-        boolean[] esPrimo = new boolean[arrSize];
-        int cuenta = 0;
-        
-     // Inicializar el array:
-        for (int i = 0; i < arrSize; i++) {
+	int maxNumber;
+	boolean[] esPrimo = new boolean[maxNumber + 1];
+	int [] vector;
+    
+	public Criba(int num) {
+		
+		maxNumber = num;
+		
+		// Inicializar el boolean array:
+        for (int i = 0; i < (maxNumber + 1); i++) {
             esPrimo[i] = true;
             // Eliminar el 0 y el 1, que no son primos:
             esPrimo[0] = esPrimo[1] = false;
         }
+        
+     // Inicializar el int array:
+        for (int i = 0; i < (maxNumber + 1); i++) {
+           vector[i] = i + 1;
+        }
+        
+        /*
+         // Inicializar el int array :
+        System.out.println("\nVector inicial hasta : " + maxNumber);
 
-        if (maxNumber >= 2) {
-
-            // Criba:
-            for (int i = 2; i < Math.sqrt(arrSize) + 1; i++) {
-                if (esPrimo[i]) {
-                    // Eliminar los múltiplos de i
-                    for (int j = 2 * i; j < arrSize; j += i) {
-                        esPrimo[j] = false;
-                    }
-                }
+        for (int i = 0; i < vector.length; i++) {
+            if (i % 10 == 0) {
+                System.out.println();
             }
-
-            // ¿Cuántos primos hay?:
-            for (int i = 0; i < arrSize; i++) {
-                if (esPrimo[i]) {
-                    cuenta++;
-                }
+            System.out.print((i + 1) + "\t");
+        }
+         */
+           
+	}
+	
+	public int cuantosPrimos () {
+		int cuenta = 0;
+		
+		// ¿Cuántos primos hay?:
+        for (int i = 0; i < (maxNumber + 1); i++) {
+            if (esPrimo[i]) {
+                cuenta++;
             }
-
-            // Rellenar el vector de números primos:
-            int[] primos = new int[cuenta];
+        }
+        
+        return cuenta;
+        
+	}
+	
+	public void ejecutarCriba() {
+		
+		// Criba:
+        for (int i = 2; i < Math.sqrt(maxNumber + 1) + 1; i++) {
+            if (esPrimo[i]) {
+                // Eliminar los múltiplos de i
+                for (int j = 2 * i; j < (maxNumber + 1); j += i) {
+                    esPrimo[j] = false;
+                }
+                
+            }
             
-            for (int i = 0, j = 0; i < arrSize; i++) {
+        }
+        
+	}
+	
+    public int[] generarPrimos() {
+    	
+    	// Generar números primos de 1 a maxNumber
+    	
+        if (maxNumber >= 2) {
+        	
+        	int count = cuantosPrimos();
+            
+            // Rellenar el vector de números primos:
+            int[] primos = new int[count];
+            
+            for (int i = 0, j = 0; i < (maxNumber + 1); i++) {
                 if (esPrimo[i]) {
                     primos[j++] = i;
                 }
@@ -54,28 +92,20 @@ public class Criba {
     }
 
     public static void main(String[] args) {
-
-        int numMaximo;
-        int[] vector;
+    	
+    	Criba criba1;
+    	int size;
         Scanner inputValue = new Scanner(System.in);
-
+        
         System.out.println("Introduce el número máximo para la criba de Erastótenes: ");
-
-        numMaximo = inputValue.nextInt();
-
-        vector = new int[numMaximo];
-
-        System.out.println("\nVector inicial hasta : " + numMaximo);
-
-        for (int i = 0; i < vector.length; i++) {
-            if (i % 10 == 0) {
-                System.out.println();
-            }
-            System.out.print((i + 1) + "\t");
-        }
-
-        vector = generarPrimos(numMaximo);
-
+        
+        size = inputValue.nextInt();
+        
+        criba1 = new Criba(size);
+        
+        System.out.println(criba1.generarPrimos());
+        
+        /*
         System.out.println("\nVector de primos hasta: " + numMaximo);
 
         for (int i = 0; i < vector.length; i++) {
@@ -84,5 +114,7 @@ public class Criba {
             }
             System.out.print(vector[i] + "\t");
         }
+        */
+        
     }
 }
